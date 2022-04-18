28e79e4b8a8295a3f8d4f105b93953c317582de7
Rafael Oliveira <101873579+rafasdoliveira@users.noreply.github.com>
2022-04-18 19:31:22 -0300

    Create URI 1007.js

---
+++ URI 1007.js
@@ -0,0 +1,13 @@
+// Diferença
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+var C = parseInt(lines.shift());
+var D = parseInt(lines.shift());
+
+var dif = (A*B - C*D);
+
+console.log("DIFERENCA = " + dif);
