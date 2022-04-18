0b766078c7720c1303c5438a19544fc115155b78
Rafael Oliveira <101873579+rafasdoliveira@users.noreply.github.com>
2022-04-18 18:38:57 -0300

    Create URI 1006.js

---
+++ URI 1006.js
@@ -0,0 +1,12 @@
+// Média 2
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var A = parseFloat(lines.shift());
+var B = parseFloat(lines.shift());
+var C = parseFloat(lines.shift());
+
+var media = ((A * 2) +(B *3) +(C * 5)) / 10;
+
+console.log("MEDIA = " + media.toFixed(1))
