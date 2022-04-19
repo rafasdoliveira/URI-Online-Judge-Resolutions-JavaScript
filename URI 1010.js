397d25a4b0f8dadbe62a2dcd7737c9cb4fc6b4f3
Rafael Oliveira <101873579+rafasdoliveira@users.noreply.github.com>
2022-04-19 11:56:11 -0300

    Create URI 1010.js

---
+++ URI 1010.js
@@ -0,0 +1,7 @@
+// Cálculo Simples
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var linha1 = parseFloat(lines.shift()).split(" ");
+var linha2 = parseFloat(lines.shift()).split(" ");
