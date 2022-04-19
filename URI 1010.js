807d3ec6d33a93c382d9e7a9f10187f7f02ef6cf
Rafael Oliveira <101873579+rafasdoliveira@users.noreply.github.com>
2022-04-19 13:37:25 -0300

    Update URI 1010.js

--- URI 1010.js
+++ URI 1010.js
@@ -3,5 +3,17 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-var linha1 = parseFloat(lines.shift()).split(" ");
-var linha2 = parseFloat(lines.shift()).split(" ");
+    var linha1 = lines.shift().split(" ");
+        var cod1 = parseInt(linha1.shift());
+        var qtd1 = parseInt(linha1.shift());
+        var val1 = parseFloat(linha1.shift());
+        var v1 = qtd1 * val1;
+        
+    var linha2 = lines.shift().split(" ");
+        var cod2 = parseInt(linha2.shift());
+        var qtd2 = parseInt(linha2.shift());
+        var val2 = parseFloat(linha2.shift());
+        var v2 = qtd2 * val2;
+        
+    var pagar = v1 + v2;
+        console.log("VALOR A PAGAR: R$ " + pagar.toFixed(2));
