120cf2d5aec1f22041771c20eb244706dcd74e0f
Rafael Oliveira <101873579+rafasdoliveira@users.noreply.github.com>
2022-04-20 20:00:17 -0300

    Update URI 1012.js

--- URI 1012.js
+++ URI 1012.js
@@ -4,19 +4,19 @@ var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 var linha1 = lines.shift().split(" ");
-    var A = parseFloat(linha1.shift());
-    var B = parseFloat(linha1.shift());
-    var C = parseFloat(linha1.shift());
+var A = parseFloat(linha1.shift());
+var B = parseFloat(linha1.shift());
+var C = parseFloat(linha1.shift());
         
-        var tri = (A * C) / 2;
-        var cir = 3.14159 * Math.pow(C,2);
-        var tap = ((A + B) * C) / 2;
-        var qua = Math.pow(B,2);
-        var ret = A * B;
+var tri = (A * C) / 2;
+var cir = 3.14159 * Math.pow(C,2);
+var tap = ((A + B) * C) / 2;
+var qua = Math.pow(B,2);
+var ret = A * B;
         
-            console.log("TRIANGULO: " + tri.toFixed(3));
-            console.log("CIRCULO: " + cir.toFixed(3));
-            console.log("TRAPEZIO: " + tap.toFixed(3));
-            console.log("QUADRADO: " + qua.toFixed(3));
-            console.log("RETANGULO: " + ret.toFixed(3));
+console.log("TRIANGULO: " + tri.toFixed(3));
+console.log("CIRCULO: " + cir.toFixed(3));
+console.log("TRAPEZIO: " + tap.toFixed(3));
+console.log("QUADRADO: " + qua.toFixed(3));
+console.log("RETANGULO: " + ret.toFixed(3));
             
