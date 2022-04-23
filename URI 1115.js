862002ab7dceff8ecbab6cb59f2658c5d7ec1471
Rafael Oliveira <101873579+rafasdoliveira@users.noreply.github.com>
2022-04-23 13:06:20 -0300

    Update URI 1115.js

--- URI 1115.js
+++ URI 1115.js
@@ -5,7 +5,7 @@ var lines = input.split('\n');
 
 let i = true;
 
-while === true{
+while (i === true) {
     var line = lines.shift().split(" ");
     var X = parseInt(line.shift());
     var Y = parseInt(line.shift());
@@ -15,6 +15,13 @@ while === true{
       } else {
           if (X > 0 && Y > 0) {
             console.log("primeiro");
-          } else if
-}
-            
+          } else if (X < 0 && Y > 0) {
+            console.log("segundo");
+          } else if (X < 0 && Y < 0) {
+             console.log("terceiro");
+          } else if (X > 0 && Y < 0) {
+              console.log("quarto")
+          }
+        }
+    }
+      
