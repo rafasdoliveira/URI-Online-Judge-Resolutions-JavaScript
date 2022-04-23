7da1816d8c6dfb3865f0fe4da889e785bcfc3dee
Rafael Oliveira <101873579+rafasdoliveira@users.noreply.github.com>
2022-04-23 09:21:07 -0300

    Create URI 1115.js

---
+++ URI 1115.js
@@ -0,0 +1,20 @@
+// Quadrante
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let i = true;
+
+while === true{
+    var line = lines.shift().split(" ");
+    var X = parseInt(line.shift());
+    var Y = parseInt(line.shift());
+  
+    if (X === 0 || Y === 0) {
+        i = false;
+      } else {
+          if (X > 0 && Y > 0) {
+            console.log("primeiro");
+          } else if
+}
+            
