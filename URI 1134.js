be17b5353d285b8a52eddace9ec5ef8e09f1dd78
Rafael Oliveira <101873579+rafasdoliveira@users.noreply.github.com>
2022-04-23 15:46:23 -0300

    Update URI 1134.js

--- URI 1134.js
+++ URI 1134.js
@@ -1,1 +1,27 @@
 // Tipo de combustível
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var alcool = 0;
+var diesel = 0;
+var gasolina = 0;
+
+var i = 1;
+
+while (i != 4) {
+   i = parseInt(lines.shift());
+    if (i === 1) {
+        alcool++;
+    } else if (i === 2) {
+        gasolina++;
+    } else if (i === 3){
+        diesel++;
+    } else if (i === 4)
+        break;
+}
+
+console.log("MUITO OBRIGADO");
+console.log("Alcool: " + alcool);
+console.log("Gasolina: " + gasolina);
+console.log("Diesel: " + diesel);
