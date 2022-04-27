e1398c9bb927cc3839b466731a65b8008659d4e4
Rafael Oliveira <101873579+rafasdoliveira@users.noreply.github.com>
2022-04-27 19:46:52 -0300

    Update URI 1017.js

--- URI 1017.js
+++ URI 1017.js
@@ -1,2 +1,11 @@
 // Gasto de combustível
 
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var time = parseInt(lines.shift());
+var velo = parseInt(lines.shift());
+var kml = 12;
+
+var litros = (velo / kml) * time;
+console.log(litros.toFixed(3));
